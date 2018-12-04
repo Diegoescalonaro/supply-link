77b9b96219dd3dde411453eafb144624d4bf8bae
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-04 23:33:06 +0100

    Added ethereum functions

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,95 +1,87 @@
 import React, { Component } from 'react';
-import ethereum from '../utils/ethereum.svg';
-import initWeb3 from './initWeb3';
-
-import platformABI from '../ethereum/ABI';
-
-import * as eth from '../ethereum/ethereumController.js';
-
+import ethereumsvg from '../utils/ethereum.svg';
 import '../css/App.css';
 
-//import Web3 from 'web3';
-//import Web3Provider from 'react-web3-provider';
-
-//ROPSTEN SMART CONTRACT: 
-//KOVAN SMART CONTRACT: 0xb8d4b1a6f55d766310641637996b59fbc4479441
-
+import initWeb3 from './initWeb3';
+import * as eth from '../ethereum/ethereumController.js';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { web3: '', defaultAccount: '0x0', contract: '', contractaddress: '0x0' }
+	}
 
-  constructor(props) {
-    super(props)
-    this.state = { web3: '', defaultAccount: '0x0', contract: '', contractaddress: '0x0' }
-  }
+	async componentDidMount() {
+		this.setState({
+			web3: await eth.web3,
+			defaultAccount: await eth.getDefaultAccount(),
+			contract: await eth.contract,
+			contractaddress: await eth.address
+		})
+	}
 
-  async componentDidMount() {
-    this.setState({
-      web3: await eth.web3,
-      defaultAccount: await eth.defaultAccount,
-      contract: await eth.contract,
-      contractaddress: await eth.address
-    })
+	start() {
+		var web3 = initWeb3();
+		var defaultAccount = web3.currentProvider.selectedAddress;
+		this.setState({
+			defaultAccount: defaultAccount,
+			web3: web3
+		})
+	}
 
-  }
+	async solicitar(_info) {
+		var x = await eth.solicitar(_info)
+		console.log(x)
+	}
 
-  start() {
-    var web3 = initWeb3();
-    var defaultAccount = web3.currentProvider.selectedAddress;
-    this.setState({
-      defaultAccount:defaultAccount,
-      web3:web3
-  })
-}
+	async getSolicitudByID(_id) {
+		var x = await eth.getSolicitudByID(_id)
+		console.log(x)
+	}
 
-  handleClick(address){
-    var trContract = new this.state.web3.eth.Contract(platformABI, address);
-    console.log(trContract)
+	async getAllSolicitudes() {
+		var x = await eth.getAllSolicitudes()
+		console.log(x)
+	}
 
-    this.setState({
-      contractaddress: address,
-      contract: trContract
-    })
-  }
+	async getAllSolicitudesByAddress(_address) {
+		var x = await eth.getAllSolicitudesByAddress(_address)
+		console.log(x)
+	}
 
-     show(){
-    // await eth.solicitar("HOHO", this.state.contract, this.state.defaultAccount).then(console.log)
-       eth.solicitar("hola")
-       
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>TFG  <code></code></h1>
+					<h4 className="text-white">Default Account (Metamask): {this.state.defaultAccount}</h4>
+					<h4 className="text-white">Smart Contract: {this.state.contractaddress}</h4>
+					<h4 className="text-white"></h4>
+
+					<img src={ethereumsvg} className="App-logo" alt="logo" />
+
+					<button className="aa" onClick={e => this.start()}>Start</button>
+
+					<input className="input" ref="input" type="text" ></input>
+				</header>
+				
+				<div className="App-body">
+					<button className="aa" onClick={e => this.solicitar(this.refs.input.value)}> SOLICITAR</button>
+					<br></br>
+					<button className="aa" onClick={e => this.getSolicitudByID(this.refs.input.value)}> getSolicitudByID</button>
+					<br></br>
+					<button className="aa" onClick={e => this.getAllSolicitudes()}> getAllSolicitudes</button>
+					<br></br>
+					<button className="aa" onClick={e => this.getAllSolicitudesByAddress(this.refs.input.value)}> getAllSolicitudesByAddress</button>
+					<br></br>
+					<h1 className="text-white">Welcome to the DAPP!</h1>
+					<br></br>      <br></br>
 
 
-  render() {
-    //initWeb3();
-
-    return (
-  
-      <div className="App">
-        <header className="App-header">
-          <img src={ethereum} className="App-logo" alt="logo" />
-          <p>
-
-            TFG  <code></code>
-          </p>
-          <button className="aa" onClick={e => this.start()}>Start</button>
-          
-          <br></br>
-          Smart contract Address: <input ref="smartcontractaddress" type="text" ></input>
-          <button className="aa" onClick={e => this.handleClick(this.refs.smartcontractaddress.value)}>Smart contract</button>
-          <br></br>
-          <button className="aa" onClick={e => this.show()}> SHOW</button>
-        </header>
-        <div className="App-body">
-          <br></br>
-          <h1 className="text-white">Welcome to the DAPP!</h1>
-          <br></br>      <br></br>
-
-          <h3 className="text-white">Default Account (Metamask): {this.state.defaultAccount}</h3>
-          <h3 className="text-white">Smart Contract: {this.state.contractaddress}</h3>
-          <h3 className="text-white"></h3>
-        </div>
-      </div>
-    );
-  }
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
