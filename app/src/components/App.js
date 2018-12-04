c280c77016ae1df7ef244b3e0401090004d38aa5
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-04 16:44:30 +0100

    Work in progres

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ethereum from '../utils/ethereum.svg';
-import initWeb3 from './web3';
+import initWeb3 from './initWeb3';
 
 import platformABI from '../ethereum/ABI';
 
-//import * as eth from '../ethereum/ethereumController.js';
+import * as eth from '../ethereum/ethereumController.js';
 
 import '../css/App.css';
 
@@ -12,18 +12,24 @@ import '../css/App.css';
 //import Web3Provider from 'react-web3-provider';
 
 //ROPSTEN SMART CONTRACT: 
-//KOVAN SMART CONTRACT: 0xd373d50883532a9e224c0d606c09c8908cb5ec42
+//KOVAN SMART CONTRACT: 0xb8d4b1a6f55d766310641637996b59fbc4479441
 
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { web3: '', defaultAccount: '0x0', contract: '', smartContractaddress: '0x0' }
+    this.state = { web3: '', defaultAccount: '0x0', contract: '', contractaddress: '0x0' }
   }
 
-  componentWillMount() {
-    initWeb3();
+  async componentDidMount() {
+    this.setState({
+      web3: await eth.web3,
+      defaultAccount: await eth.defaultAccount,
+      contract: await eth.contract,
+      contractaddress: await eth.address
+    })
+
   }
 
   start() {
@@ -43,7 +49,12 @@ class App extends Component {
       contractaddress: address,
       contract: trContract
     })
-    
+  }
+
+     show(){
+    // await eth.solicitar("HOHO", this.state.contract, this.state.defaultAccount).then(console.log)
+       eth.solicitar("hola")
+       
   }
 
 
@@ -51,8 +62,7 @@ class App extends Component {
     //initWeb3();
 
     return (
-      
-
+  
       <div className="App">
         <header className="App-header">
           <img src={ethereum} className="App-logo" alt="logo" />
@@ -65,8 +75,9 @@ class App extends Component {
           <br></br>
           Smart contract Address: <input ref="smartcontractaddress" type="text" ></input>
           <button className="aa" onClick={e => this.handleClick(this.refs.smartcontractaddress.value)}>Smart contract</button>
+          <br></br>
+          <button className="aa" onClick={e => this.show()}> SHOW</button>
         </header>
-
         <div className="App-body">
           <br></br>
           <h1 className="text-white">Welcome to the DAPP!</h1>
@@ -74,7 +85,7 @@ class App extends Component {
 
           <h3 className="text-white">Default Account (Metamask): {this.state.defaultAccount}</h3>
           <h3 className="text-white">Smart Contract: {this.state.contractaddress}</h3>
-
+          <h3 className="text-white"></h3>
         </div>
       </div>
     );
