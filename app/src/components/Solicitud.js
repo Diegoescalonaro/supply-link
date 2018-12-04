db1d7c091bbe418f4d8f9b1bcc51eb13b62bb5ff
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-04 16:42:43 +0100

    Added new component

---
+++ app/src/components/Solicitud.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Solicitud extends Component {
+
+    constructor(props) {
+      super(props)
+      this.state = { web3: '', defaultAccount: '0x0', contract: '', smartContractaddress: '0x0' }
+    }
+
+
+
+
+
+}
+
