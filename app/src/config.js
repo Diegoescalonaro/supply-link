321c589c7f8b1fe76e1bd91802fdacd684580406
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-08 16:28:10 +0100

    Added config variable

---
+++ app/src/config.js
@@ -0,0 +1,8 @@
+var config ={
+    network: 'Kovan',
+    smartcontractaddress: '0x195a727831ab80300d6515ff9aad5ad7c7aa88f3',
+    
+    
+    
+}
+export default config
--- app/src/utils/config.js
+++
@@ -1,6 +0,0 @@
-var config ={
-    network: "Kovan",
-    
-    
-}
-export default config
