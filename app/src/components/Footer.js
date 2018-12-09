594ba909add7aaa844ae6e79018d6957fba9dadc
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-09 20:10:28 +0100

    Added footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 export default class Footer extends Component {
+    constructor(props) {
+        super(props)
+    }
 
+    render() {
+        console.log("* * Component HEADER Render * *")
 
+        return (
+            <div className="footer">
+                <br></br>
+                <h3 className="head">- Footer -</h3>
+                <br></br>
+            </div>
 
+        )
+    }
 
 }
