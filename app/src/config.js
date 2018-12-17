8141549fefb58ac16dfb6c6c35de493b5aea17e3
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2018-12-17 22:03:06 +0100

    Moved to Rinkeby testnet

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,9 @@
 var config ={
     network: 'Kovan',
-    smartcontractaddress: '0x257b195a3c5cd78a1238818ad9baa7ad9f24383a',   
+    smartcontractaddress: '0x96D1374D24D90FaEb11413365D28482a26C63791',   
 }
+
+
+//smartcontract Kovan: 0x257b195a3c5cd78a1238818ad9baa7ad9f24383a
+//smartcontract Rinkeby: 0x96D1374D24D90FaEb11413365D28482a26C63791
 export default config
