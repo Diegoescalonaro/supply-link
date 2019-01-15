0d3a0ae28fee2852823f6f218f7134da10d30f2e
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2019-01-15 16:46:43 +0100

    Uploaded

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -12,7 +12,7 @@ export default class Footer extends Component {
         return (
             <div className="footer">
                 <br></br>
-                <h3 className="head">- Footer -</h3>
+                <h3 className="head"></h3>
                 <p className="lead">Trabajo Fin De Grado - Supply-Link</p>
                 <br></br>
             </div>
